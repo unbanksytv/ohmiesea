58568da68a08ae65e511b6e605fcabc00cc3428a
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-14 04:35:10 +0200

    first commit

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 33,
+    from: 'GoodVibesOhmly',
+    to: 'LiveTheLifeTv',
+    date: '3 months ago',
+  },
+  {
+    price: 33,
+    from: 'GoodVibesOhmly',
+    to: 'LiveTheLifeTv',
+    date: '3 months ago',
+  },
+  {
+    price: 33,
+    from: 'GoodVibesOhmly',
+    to: 'LiveTheLifeTv',
+    date: '3 months ago',
+  },
+  {
+    price: 33,
+    from: 'GoodVibesOhmly',
+    to: 'LiveTheLifeTv',
+    date: '3 months ago',
+  },
+]
