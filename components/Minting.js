6874f2cf3a839608bf171cce871819e872fbd842
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-14 12:38:51 +0200

    Button/Main/Minting comp , pack json, and css mod

---
+++ components/Minting.js
@@ -0,0 +1,132 @@
+import { useEffect, useState } from "react";
+import tw from "tailwind-styled-components";
+import { useAddress, useDisconnect, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
+import ReactLoading from "react-loading";
+import { toast } from "react-toastify";
+import { useRouter } from 'next/router'
+import { Button } from "./Button";
+
+const Minting = () => {
+  const [totalSupply, setTotalSupply] = useState(0);
+  const [inProgress, setInProgress] = useState(false);
+  const [completed, setCompleted] = useState(false);
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  const disconnectWallet = useDisconnect();
+  const EditionDrop = useEditionDrop("0xE409914e454b133B61a404345f232c1eB487cC61")
+  const router = useRouter()
+
+  const mint = async () => {
+    if(EditionDrop && address) {
+      setInProgress(true);
+      try {
+        await EditionDrop.claimTo(address, 0, 1);
+        setInProgress(false);
+        setCompleted(true);
+        router.push('/success')
+        toast.success('🦄 Mint Successful! LFG')
+      } catch (error) {
+        console.log(error)
+        setInProgress(false)
+        setCompleted(false)
+        toast.error('Looks like you do are not on the LTL Allowlist to claim a free NFT.')
+      }
+    }
+  }
+
+  useEffect(() => {
+    const getTotal = async () => {
+      if(EditionDrop) {
+        const total = await EditionDrop.totalSupply(0);
+        setTotalSupply(total.toNumber());
+      }
+    }
+    getTotal();
+  }, [EditionDrop])
+
+  return (
+    <Container>
+      <Mint>
+        <TitleContainer>
+          <Title>The Photo Labs <br /> by LiveTheLifeTV<br /> Claim Your NFT.</Title>
+        </TitleContainer>
+        <ButtonContainer>
+            {
+              address
+                ? <>
+                {
+                  !completed &&
+                    <Button
+                      disabled={inProgress}
+                      onclick={mint}
+                    >
+                      {
+                        inProgress
+                        ? <ReactLoading type="bubbles" color="#000" height={64} />
+                        : <>Mint</>
+                      }
+                    </Button>
+                }
+                <Button
+                  style='ghost'
+                  disabled={inProgress}
+                  onclick={disconnectWallet}
+                >
+                  Disconnect
+                </Button>
+                </>
+                :<Button onclick={connectWithMetamask}>
+                  Connect Wallet
+                </Button>
+            }
+        </ButtonContainer>
+      </Mint>
+    </Container>
+  )
+}
+
+export default Minting
+
+const Count = tw.div`
+ flex
+ grow
+ items-center
+ justify-center
+`
+
+const ButtonContainer = tw.div`
+ mt-2
+ gap-4
+ flex
+`
+
+const Mint = tw.div`
+ max-w-screen-sm
+ lg:w-1/3
+ md:w-1/2
+ bg-black
+ lg:mt-[-200px]
+ z-50
+ flex
+ flex-col
+ pb-4
+ pr-4
+`
+
+const Title = tw.h2`
+ uppercase
+ text-3xl
+ italic
+ font-bold
+ mt-3
+`
+
+const TitleContainer = tw.div`
+ flex
+`
+
+const Container = tw.div`
+ max-w-screen-lg
+ w-full
+ z-50
+`
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 import Head from 'next/head'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Footer from '../components/Footer'
+import tw from "tailwind-styled-components";
 import { useWeb3 } from '@3rdweb/hooks'
 import { useEffect } from 'react'
 import { client } from '../lib/sanityClient'
 import toast, { Toaster } from 'react-hot-toast'
-
-const style = {
-  wrapper: ``,
-  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
-  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
-  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
-}
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Button from "../components/Button";
+import Footer from "../components/Footer";
 
 export default function Home() {
   const { address, connectWallet } = useWeb3()
@@ -46,28 +41,28 @@ export default function Home() {
   }, [address])
 
   return (
-    <div className={style.wrapper}>
-      <Toaster position="top-center" reverseOrder={false} />
-      {address ? (
-        <>
-          <Header />
-          <Hero />
-          <Footer />
-        </>
-      ) : (
-        <div className={style.walletConnectWrapper}>
-          <button
-            className={style.button}
-            onClick={() => connectWallet('injected')}
-          >
-            Connect Wallet
-          </button>
-          <div className={style.details}>
-            You need Good Vibes to be
-            <br /> able to run this app.
-          </div>
-        </div>
-      )}
-    </div>
+    <Container>
+      <Head>
+        <title>The Photo Labs</title>
+        <meta name="description" content="Made with love by LiveTheLifeTV" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Main />
+      <Button />
+      <Hero />
+      <Footer />
+     </Container>
   )
 }
+
+const Container = tw.div`
+ w-screen
+ h-screen
+ bg-black
+ text-white
+ px-7
+ flex
+ flex-col
+ justify-between
+`
